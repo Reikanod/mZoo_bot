f97071a45295d9590e31f00cb55eede730c8c3fe
Reikanod <reikanod@yandex.ru>
2024-08-09 15:40:10 +0300

    just save

--- test2.py
+++ test2.py
@@ -1,4 +1,13 @@
-with open(r'../ya_api_keys.txt', 'r', encoding='utf-8') as file:
-    file_lines = file.readlines()
-    cat_id = file_lines[1].strip()
-    ya_api_id = file_lines[5].strip()
+def rub():
+    return 'Рублей: '
+
+def dol():
+    return 'Долларов: '
+
+
+def summ(func):
+    a = 10
+    return f'func() + {a}'
+
+
+print(rub)
