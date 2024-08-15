b514564abb01f58886d8611d7c681247a45cdb9c
Reikanod <reikanod@yandex.ru>
2024-08-15 15:04:12 +0300

    just save

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import telebot.types
+
 from connect_to_bot import bot
 from states import State, StateStart
-
+from quiz import list_of_questions
 
 state = State()
 state.cur_state = StateStart()
@@ -9,16 +11,35 @@ state.cur_state = StateStart()
 @bot.message_handler(commands=['start'])
 def greet(message):
     state.cur_state = 'start'
-
 def greet_dialog(message):
     pass
+
+
+@bot.message_handler(commands=['quiz'])
+def quiz(message):
+    state.cur_state = 'quiz'
+    keyboard = telebot.types.ReplyKeyboardMarkup()
+    button_start_quiz = telebot.types.KeyboardButton('Начать викторину')
+    keyboard.add(button_start_quiz)
+    bot.send_message(message.chat.id, "Ответьте на вопросы и мы подберем ваше тотемное животной из нашего зоопарка", reply_markup=keyboard)
+
+def quiz_dialog(message):
+    questions = list_of_questions.copy()
+    question = questions.popitem()
+    keyboard = telebot.types.ReplyKeyboardMarkup()
+    for answer in question:
+        telebot.types.KeyboardButton(answer)
+        keyboard.add(answer)
+
+
 
 @bot.message_handler(content_types=['text'])
 def state(message):
     match state.cur_state:
         case 'start':
             greet_dialog(message)
-
+        case 'quiz':
+            quiz_dialog(message)
 
 
 
--- quiz.py
+++ quiz.py
@@ -1,24 +1,16 @@
 # здесь будут собраны все вопросы для викторины
-path_to_questions = r'questions.txt'
 
 
-class Quiz():
-    def __init__(self):
-        self.path_to_questions = path_to_questions
-        self.list_of_questions = {}
-        # создается словарь из вопросов и ответов к нему
 
-    def read_questions(self):  # обновляет список вопросов, считывая их из файла
-        with open(path_to_questions, 'r') as file:
-            for line in file:
-                if line[0] == '#' or not line:
-                    continue
-                
+list_of_questions = {}        # создается словарь строка вопросов и список ответов к нему
+with open(r'questions.txt', 'r', encoding='utf-8') as file:
+    count = 1
+    for line in file:
+        if count == 1:
+            prev_line = line
+            count += 1
+        else:
+            list_of_questions[prev_line.strip()] = line.split(';')
+            count = 1
 
-class Questions():
-    def __init__(self):
-        self.question = ''
-        self.answers = []
-        # считать вопросы из файла
 
-    def # функция, которая результаты ответов пользователя
